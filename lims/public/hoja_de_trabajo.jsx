f5f299221c17a016858ba18349e47e4cfe7bd917
Metzly <132518143+Metzly@users.noreply.github.com>
2024-10-15 21:08:28 -0400

    rutas

--- lims/public/hoja_de_trabajo.jsx
+++ lims/public/hoja_de_trabajo.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Menu from './menu.jsx'
+import Menu from '/src/menu.jsx'
 import HojaTrabajoAnalista from './hojaTrabajoAnalista';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './index.css'
-import './menu.css'
+import '/src/index.css'
+import '/src/menu.css'
 
 createRoot(document.getElementById('hojaTrabajo')).render(
   <StrictMode>
--- lims/public/inicio_analista.jsx
+++ lims/public/inicio_analista.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Menu from './menu.jsx'
+import Menu from '/src/menu.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './index.css'
-import './menu.css'
+import '/src/index.css'
+import '/src/menu.css'
 
 createRoot(document.getElementById('inicioAnalista')).render(
   <StrictMode>
