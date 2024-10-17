9049f112166eedf697239e276fe1190b34d4a221
Odett <1114929@est.intec.edu.do>
2024-10-17 10:48:03 -0400

    pantalla de login

---
+++ lims/public/Inicio_Sesion.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from '/src/App.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '/src/index.css'
+import '/src/menu.css'
+
+createRoot(document.getElementById('Inicio_Sesion')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
