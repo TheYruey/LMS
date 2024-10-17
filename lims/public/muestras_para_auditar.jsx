8a801c95f617f1632c0d3085a4da4ce4eaced078
Odett <1114929@est.intec.edu.do>
2024-10-17 00:25:26 -0400

    modal de ver enmiendas nicompleto

--- lims/public/muestras_para_auditar.jsx
+++ lims/public/muestras_para_auditar.jsx
@@ -9,6 +9,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Button, Typography } from '@mui/material';
+import ModalEnmiendas from '../src/modalenmiendas';
 
 createRoot(document.getElementById('muestras_para_auditar')).render(
   <StrictMode>
@@ -22,6 +23,7 @@ createRoot(document.getElementById('muestras_para_auditar')).render(
             <Button variant='contained' sx={{backgroundColor: "#12C2E9"}}>Refrescar</Button>
             <ModalEnmendar />
             <ModalAuditar />
+            <ModalEnmiendas/>
         </div>
         <TableComponent />
       </div>
---
+++ lims/src/modalEnmiendas.jsx
@@ -0,0 +1,72 @@
+import './modalEnmiendas.css';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import React from 'react';
+import EnmiendasTable from './TableComponent'; 
+
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: "700px",
+    bgcolor: 'background.paper',
+    borderRadius: 3,
+    p: 6,
+    display: "flex",
+    flexDirection: "column",
+};
+
+export default function ModalEnmiendas(){
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
+    return (
+        <div>
+            <Button onClick={handleOpen} variant='contained' sx={{ backgroundColor: "#12C2E9" }}>
+                Lista de enmiendas
+            </Button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <div id="top" style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                            <Button
+                                variant="contained"
+                                onClick={handleClose}
+                                sx={{ height: 50, borderRadius: 15, marginRight: 2, backgroundColor: "#12C2E9" }}
+                            >
+                                <ArrowBackIcon />
+                            </Button>
+                            <Typography id="transition-modal-title" variant="h6" component="h2">
+                                Asignar analista
+                            </Typography>
+                        </div>
+                        
+                        {/* Aquí se incluye el TableComponent */}
+                        <EnmiendasTable />
+
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}
