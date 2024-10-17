25866cfe09932fdbb8098ad8510def8f29a2b381
Metzly <132518143+Metzly@users.noreply.github.com>
2024-10-17 19:10:19 -0400

    .

--- lims/src/main.jsx
+++ lims/src/main.jsx
@@ -1,31 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.jsx'
-import MenuAnalista from '../src/menu_analista.jsx';
-//import ModalEnmendar from './modalEnmendar';
-//import ModalAuditar from './modalAuditar';
-//import ModalAsignarAnalista from './modalAsignarAnalista';
-//import ModalDatosUsuario from './modalDatosUsuario';
-//import ModalEditarAnalisis from './modalEditarAnalisis';
-//import FormEntidadesExternas from './formEntidadesExternas';
-//import ModalEditar from './modalEditar';
-//import ListaFormasFarmaceuticas from './listaFormasFarmaceuticas';
-//import ListaPermisos from './listaPermisos';
-//import ListaDocumentos from './listaDocumentos';
-//import EditarRol from './editarRol';
+import App from './App.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import './index.css'
 import './menu.css'
-import AnalysisForm from './AnalysisForm.jsx';
 
-createRoot(document.getElementById('Hoja_Trabajo')).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-      <MenuAnalista />
-      <AnalysisForm />
-    </div>
+      <App />
   </StrictMode>,
 )
