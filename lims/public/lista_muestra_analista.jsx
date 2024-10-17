b95f5e1af87f1b352421c7a2be2400f57c8542e5
Odett <1114929@est.intec.edu.do>
2024-10-17 17:08:55 -0400

    m

--- lims/public/lista_muestra_analista.jsx
+++ lims/public/lista_muestra_analista.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Menu from '/src/menu.jsx'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '/src/menu.css'
 import TableComponent from '../src/TableComponent';
-import { Button, Typography } from '@mui/material';
+import { Typography } from '@mui/material';
+import MenuAnalista from '../src/menu_analista';
 createRoot(document.getElementById('lista_muestra_analista')).render(
   <StrictMode>
   <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-    <Menu />
+    <MenuAnalista />
     <div style={{width: "100%", marginLeft: "20%", marginTop: "4%"}}>
       <Typography Typography id="transition-modal-title" variant="h5">
           Historial de muestras
